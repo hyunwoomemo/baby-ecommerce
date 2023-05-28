e0cdf11c9f5f72617f67e30a4acc09d7840aaeec
hyunwoomemo <archihw94@gmail.com>
2023-05-29 05:56:06 +0900

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import Product from "./pages/Admin/Product";
+import ProductList from "./pages/Product/ProductList";
+import ProductDetails from "./pages/Product/ProductDetail";
 
 const queryClient = new QueryClient()
 
@@ -22,6 +24,8 @@ function App() {
             <Route path="/login" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
             <Route exact path="/admin" element={<Admin />}></Route>
+            <Route exact path="/products" element={<ProductList />}></Route>
+            <Route path="/products/:id" element={<ProductDetails />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
             <Route exact path="/admin/product" element={<Product />}></Route>
           </Routes>
